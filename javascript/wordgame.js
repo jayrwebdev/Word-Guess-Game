394a6f20ae138d72f621d0a5f89cee178be46918
Jahaan Rasheed <jahaanrasheed@Jahaans-MacBook-Pro.local>
2019-07-01 22:04:48 -0400

    changes

--- javascript/wordgame.js
+++ javascript/wordgame.js
@@ -1,29 +1,38 @@
 var songStorage = ["chopper", "mural", "stronger", "deliver", "fighters", "real", "manilla"];
-var currentWord 
+var currentWord
 var wins = 0
 var guessCounter = 0
 var rightLetters = [];
 var wrongLetters = [];
 
+ 
+
 document.onkeyup = function (event) {
-       //var currentWord = "";
 
        if (guessCounter == 0) {
               guessCounter = 12;
               rightLetters = []
               wrongLetters = []
-
+   
               // Select Array from WordBank
-              var select = 3
               //Math.floor(Math.floor() * songStorage.length);
-              currentWord = songStorage[Math.floor(Math.random()*songStorage.length)];
-               
-              
-       }
+              currentWord = songStorage[Math.floor(Math.random() * songStorage.length)];
+          }
+          //creates Word order for current
+         var currentWordArray = currentWord.split("")
+         var underscoreArr = [];
+   
+         for (i = 0; i < currentWordArray.length; i++) {
+             underscoreArr.push("_")
+         }
+       //var currentWord = "";
+
 
        console.log(currentWord)
 
        var index = currentWord.indexOf(event.key.toLowerCase())
+
+
        // Check if index is -1,  add wrongLetters array,
        if (index === -1) {
               wrongLetters.push(event.key);
@@ -31,36 +40,38 @@ document.onkeyup = function (event) {
        }
        // Pushes correct Letters to rightLetters Array
        if (index > -1) {
-             rightLetters.push(event.key);
-             alert("Correct Word")
+              rightLetters.push(event.key);
+              alert("Correct Word")
        }
        console.log(index);
-       guessCounter = guessCounter -1
+       guessCounter = guessCounter - 1
        // correct letters = correct word add 1 point to wins
-           
-           console.log(rightLetters)
-           console.log(currentWord)
 
-          if (rightLetters.length > 0) {
-                 if (rightLetters.join("") === currentWord) {
-                 wins = wins + 1
-                 $("currentword").html(currentWord);
-            }
-          }
-          
+       console.log(rightLetters)
+       console.log(currentWord)
 
-      
-       
-      
-       
+       if (rightLetters.length > 0) {
+              if (rightLetters.join("") === currentWord) {
+                     wins = wins + 1
+                     $("currentword").html(currentWord);
+                     reset
+              }
+       }
 
-       
+
+
+
+
+
+
+
 
        $("#rightletters").html(rightLetters);
        $("#wrongletters").html(wrongLetters);
        $("#guesscounter").html(guessCounter);
        $("#wins").html(wins);
-      
+       $("#currentword").html(underscoreArr);
+
        // if wrong letter, display wrong letter, *
        //everytime user presses letter -1 (guess),*
        // if the key id correct display in right word array,*
